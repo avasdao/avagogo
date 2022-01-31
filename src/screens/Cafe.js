f0023bedd5196c4dda079683871a4638d4a69291
Shomari <nyusternie@sdot.io>
2022-01-31 01:45:07 -0500

    Add Gifted Chat to cafe.

--- src/screens/Cafe.js
+++ src/screens/Cafe.js
@@ -11,6 +11,7 @@ import React from 'react'
 import type {Node} from 'react'
 
 import {
+  Pressable,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -20,112 +21,112 @@ import {
   View,
 } from 'react-native'
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen'
-
 import Ionicons from 'react-native-vector-icons/Ionicons'
 
 import tailwind from 'tailwind-rn'
 
 import LottieView from 'lottie-react-native'
 
-const Section = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark'
-
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+import { GiftedChat } from 'react-native-gifted-chat'
 
 /**
- * Home Screen
+ * Cafe Screen
  */
-function Home() {
-    /* Request dard mode. */
-    const isDarkMode = useColorScheme() === 'dark'
+function Cafe() {
+    const [hasAgreed, setHasAgreed] = React.useState(false)
+    const [messages, setMessages] = React.useState([])
 
-    /* Set background style. */
-    const backgroundStyle = {
-        backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    React.useEffect(() => {
+        setMessages([
+            {
+                _id: 1,
+                text: 'Hello developer',
+                createdAt: new Date(),
+                user: {
+                    _id: 2,
+                    name: 'React Native',
+                    avatar: 'https://placeimg.com/140/140/any',
+                },
+            },
+        ])
+    }, [])
+
+    const onSend = React.useCallback((messages = []) => {
+        setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
+    }, [])
+
+    if (hasAgreed) {
+        return (
+            <GiftedChat
+                messages={messages}
+                onSend={messages => onSend(messages)}
+                user={{
+                    _id: 1,
+                }}
+            />
+        )
     }
 
-    return (
-        <ScrollView
-            contentInsetAdjustmentBehavior="automatic"
-            style={tailwind('')}
-        >
-            <Section title="Cafe">
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-            </Section>
+    if (!hasAgreed) {
+        return (
+            <ScrollView
+                contentInsetAdjustmentBehavior="automatic"
+                style={tailwind('')}
+            >
+                <View>
+                    <View style={tailwind('px-5 pt-5 items-center')}>
+                        <Text style={tailwind('text-lg text-gray-800 font-bold')}>
+                            Meet the #1 DeFi community in crypto!
+                        </Text>
 
-            <View style={tailwind('py-6 items-center')}>
-                <View style={tailwind('bg-pink-200 px-3 py-2 rounded-full')}>
-                    <Text style={tailwind('text-pink-800 text-xl font-semibold')}>
-                        1,337 traders online..
-                    </Text>
+                        <Text style={tailwind('mt-3 text-lg text-gray-800')}>
+                            You can now engage with other investors like you in a <Text style={tailwind('font-bold')}>SAFE & FRIENDLY</Text> environment.
+                        </Text>
+                    </View>
+
+                    <View style={tailwind('mt-5 py-5 bg-white items-center border-t-2 border-b-2 border-pink-500')}>
+                        <LottieView
+                            style={tailwind('h-48')}
+                            source={require('../assets/lottie/couple-talk.json')} autoPlay loop
+                        />
+
+                        <Text style={tailwind('text-pink-500 font-semibold')}>
+                            Ava GoGo's 24 Hour Cafe
+                        </Text>
+                    </View>
+
+                    <View style={tailwind('px-5 pt-5')}>
+                        <Text style={tailwind('text-sm text-red-500 font-bold')}>
+                            !! WARNING !!
+                        </Text>
+
+                        <Text style={tailwind('mt-3 text-sm text-gray-800')}>
+                            This is a very early (alpha) release of Ava GoGo that is without moderation or any tools to prevent <Text style={tailwind('font-bold')}>ABUSE & SPAM</Text> messages.
+                        </Text>
+
+                        <Text style={tailwind('mt-3 text-sm text-gray-800')}>
+                            We hope you will <Text style={tailwind('font-bold')}>behave yourselves and act responsibly</Text> while our team continues it work to enhance this social experience.
+                        </Text>
+
+                        <Text style={tailwind('mt-3 text-sm text-red-500 font-bold')}>
+                            !! USE AT YOUR OWN RISK !!
+                        </Text>
+                    </View>
+
+                    <View style={tailwind('py-6 items-center')}>
+                        <Pressable
+                            onPress={() => setHasAgreed(true)}
+                            style={tailwind('bg-yellow-200 px-10 py-2 border-2 border-yellow-400 rounded-xl')}
+                        >
+                            <Text style={tailwind('text-yellow-800 text-xl font-semibold')}>
+                                Okay, got it!
+                            </Text>
+                        </Pressable>
+                    </View>
                 </View>
-            </View>
-
-            <View style={tailwind('py-5 bg-gray-50 items-center')}>
-                <LottieView
-                    style={tailwind('h-48')}
-                    source={require('../assets/lottie/couple-talk.json')} autoPlay loop
-                />
-
-                <Text style={tailwind('text-pink-500 font-semibold')}>
-                    24 Hour Cafe
-                </Text>
-            </View>
-
-            <Section title="What's Next?">
-                Let's build the greatest DeFi app EVER!!
-            </Section>
-        </ScrollView>
-    )
+            </ScrollView>
+        )
+    }
 }
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default Home
+export default Cafe
