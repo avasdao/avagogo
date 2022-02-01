fe9153367a55611a310ef962fa3ea4170a43c450
Shomari <nyusternie@sdot.io>
2022-02-01 16:05:03 -0500

    Add tabs to Alpha labs.

--- src/screens/DApps/AlphaFinanceLabs.js
+++ src/screens/DApps/AlphaFinanceLabs.js
@@ -21,11 +21,73 @@ import {
   View,
 } from 'react-native'
 
+import {
+    createMaterialTopTabNavigator
+} from '@react-navigation/material-top-tabs'
+
 import Ionicons from 'react-native-vector-icons/Ionicons'
 
 import tailwind from 'tailwind-rn'
 
 import LottieView from 'lottie-react-native'
+
+/* Initialize tab (navigation). */
+const Tab = createMaterialTopTabNavigator()
+
+/**
+ * Yield
+ */
+const Yield = () => {
+    return (
+        <ScrollView
+            contentInsetAdjustmentBehavior="automatic"
+            style={tailwind('')}
+        >
+        <View style={tailwind('m-3')}>
+
+            <View style={tailwind('mt-3 px-5 py-2 flex flex-row justify-between border-2 border-gray-500 rounded-xl')}>
+                <View style={tailwind('flex flex-row items-center')}>
+                    <Image
+                        style={tailwind('w-4 h-4 mr-1')}
+                        source={require('../../assets/tokens/avax.png')}
+                    />
+                    <Image
+                        style={tailwind('w-4 h-4 mr-1 relative -ml-2')}
+                        source={require('../../assets/tokens/usdt.png')}
+                    />
+                    <View>
+                        <Text style={tailwind('text-xs')}>Trader Joe</Text>
+                        <Text style={tailwind('text-xs')}>AVAX/USDT.e</Text>
+                    </View>
+                </View>
+                <Text>$210,878,984</Text>
+                <Text>$69.98</Text>
+                <Text>-0.21%</Text>
+                <Text>3.03%</Text>
+            </View>
+
+        </View>
+        </ScrollView>
+    )
+}
+
+/**
+ * Liquidity
+ */
+const Liquidity = () => {
+    return (
+        <ScrollView
+            contentInsetAdjustmentBehavior="automatic"
+            style={tailwind('')}
+        >
+            <View style={tailwind('m-3')}>
+                <Text style={tailwind('text-red-500 text-lg font-medium')}>
+                    There are no liquidity providers.
+                </Text>
+            </View>
+        </ScrollView>
+    )
+}
 
 /**
  * DApp Screen
@@ -44,135 +106,96 @@ function DApp({navigation}) {
         fetchInfo()
     }, [])
 
-    const MainView = () => {
-        return (
-            <ScrollView
-                contentInsetAdjustmentBehavior="automatic"
-                style={tailwind('')}
-            >
-                <View style={tailwind('flex flex-row justify-between')}>
-
-                    <View style={tailwind('w-6/12 my-3 ml-3 p-3 bg-gray-700 opacity-80 rounded-xl')}>
-                        <View style={tailwind('flex flex-row items-center')}>
-                            <Ionicons
-                                style={tailwind('mr-2 text-gray-300')}
-                                name={'lock-closed-outline'}
-                                size={30}
-                                color={'rgba(90, 90, 90, 0.8)'}
-                            />
-
-                            <Text style={tailwind('text-gray-200 font-bold text-lg uppercase')}>
-                                Total Value Locked
-                            </Text>
-                        </View>
-
-                        <Text style={tailwind('mt-5 text-gray-50 text-2xl font-bold text-center')}>
-                            $465,244,384
-                        </Text>
-                    </View>
-
-                    <View style={tailwind('w-5/12 m-2 p-3 bg-gray-700 opacity-80 rounded-xl')}>
-                        <View style={tailwind('flex flex-row items-center')}>
-                            <Ionicons
-                                style={tailwind('mr-2 text-gray-300')}
-                                name={'globe-outline'}
-                                size={30}
-                                color={'rgba(90, 90, 90, 0.8)'}
-                            />
-
-                            <Text style={tailwind('text-gray-200 font-bold text-lg uppercase')}>
-                                Global
-                            </Text>
-                        </View>
-
-                        <View style={tailwind('mt-3')}>
-                            <Text style={tailwind('text-gray-300 text-sm font-medium')}>
-                                Total Collateral
-                            </Text>
-
-                            <Text style={tailwind('text-gray-50 text-base font-bold text-right')}>
-                                $255,022,498
-                            </Text>
-                        </View>
-
-                        <View style={tailwind('mt-3')}>
-                            <Text style={tailwind('text-gray-300 text-sm font-medium')}>
-                                Total Borrow
-                            </Text>
-
-                            <Text style={tailwind('text-gray-50 text-base font-bold text-right')}>
-                                $149,184,228
-                            </Text>
-                        </View>
-
-                        <View style={tailwind('mt-3')}>
-                            <Text style={tailwind('text-gray-300 text-sm font-medium')}>
-                                Active Positions
-                            </Text>
-
-                            <Text style={tailwind('text-gray-50 text-base font-bold text-right')}>
-                                865 Positions
-                            </Text>
-                        </View>
-                    </View>
-
-                </View>
-
-
-                <View style={tailwind('w-full m-3 p-3 bg-gray-700 opacity-80 rounded-xl items-center')}>
-                    <View style={tailwind('flex flex-row items-center')}>
-                        <Ionicons
-                            style={tailwind('mr-2 text-gray-300')}
-                            name={'lock-closed-outline'}
-                            size={20}
-                            color={'rgba(90, 90, 90, 0.8)'}
-                        />
-
-                        <Text style={tailwind('text-gray-200 font-medium')}>
-                            Farm Pools (12 Pools)
-                        </Text>
-                    </View>
-
-                    <Text style={tailwind('mt-2 text-gray-50 text-lg')}>
-                        ALL | Yield Farming | Liquidity Providing
-                    </Text>
-                </View>
-            </ScrollView>
-        )
-    }
-
     return (
-        <View>
+        <View style={tailwind('h-full')}>
             <Image
                 style={tailwind('w-full absolute')}
                 source={require('../../assets/backgrounds/gradient-purple-pink.jpg')}
             />
 
-            <MainView />
+            <View style={tailwind('flex flex-row justify-between')}>
+
+                <View style={tailwind('w-6/12 my-3 ml-3 p-3 bg-gray-700 opacity-80 rounded-xl')}>
+                    <View style={tailwind('flex flex-row items-center')}>
+                        <Ionicons
+                            style={tailwind('mr-2 text-gray-300')}
+                            name={'lock-closed-outline'}
+                            size={30}
+                        />
+
+                        <Text style={tailwind('text-yellow-300 font-bold text-lg text-center uppercase')}>
+                            Total Value Locked
+                        </Text>
+                    </View>
+
+                    <Text style={tailwind('mt-5 text-gray-50 text-2xl font-bold text-center')}>
+                        $465,244,384
+                    </Text>
+                </View>
+
+                <View style={tailwind('w-5/12 m-2 p-3 bg-gray-700 opacity-80 rounded-xl')}>
+                    <View style={tailwind('flex flex-row items-center')}>
+                        <Ionicons
+                            style={tailwind('mr-2 text-gray-300')}
+                            name={'globe-outline'}
+                            size={30}
+                        />
+
+                        <Text style={tailwind('text-yellow-300 font-bold text-lg text-center uppercase')}>
+                            Global Markets
+                        </Text>
+                    </View>
+
+                    <View style={tailwind('mt-3')}>
+                        <Text style={tailwind('text-gray-300 text-sm font-medium')}>
+                            Total Collateral
+                        </Text>
+
+                        <Text style={tailwind('text-gray-50 text-base font-bold text-right')}>
+                            $255,022,498
+                        </Text>
+                    </View>
+
+                    <View style={tailwind('mt-3')}>
+                        <Text style={tailwind('text-gray-300 text-sm font-medium')}>
+                            Total Borrow
+                        </Text>
+
+                        <Text style={tailwind('text-gray-50 text-base font-bold text-right')}>
+                            $149,184,228
+                        </Text>
+                    </View>
+
+                    <View style={tailwind('mt-3')}>
+                        <Text style={tailwind('text-gray-300 text-sm font-medium')}>
+                            Active Positions
+                        </Text>
+
+                        <Text style={tailwind('text-gray-50 text-base font-bold text-right')}>
+                            865 Positions
+                        </Text>
+                    </View>
+                </View>
+            </View>
+
+            <Tab.Navigator style={tailwind('')}>
+                <Tab.Screen
+                    name="Yield"
+                    component={Yield}
+                    options={{
+                        title: 'Yield Farming'
+                    }}
+                />
+                <Tab.Screen
+                    name="Liquidity"
+                    component={Liquidity}
+                    options={{
+                        title: 'Liquidity'
+                    }}
+                />
+            </Tab.Navigator>
         </View>
     )
 }
-
-const styles = StyleSheet.create({
-    background: {
-        // position: 'absolute',
-        // top: 0,
-        // bottom: 0,
-        // backgroundImage: 'linear-gradient(red, orange)',
-        // width: Dimensions.get('window').width,
-        // height: Dimensions.get('window').height,
-        // opacity: 0.5,
-    },
-    header: {
-        position: 'absolute',
-        width: Dimensions.get('window').width,
-        height: 200,
-        opacity: 0.2,
-    },
-    menuBtn: {
-        width: (Dimensions.get('window').width / 2) - 30,
-        height: 150,
-    },
-})
 
 export default DApp
